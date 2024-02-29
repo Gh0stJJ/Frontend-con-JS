956ee07bf4593cae00c5e04107bcd243888fdb05
Juan Valdiviezo <valdiviezojuan7@gmail.com>
2024-02-29 17:51:10 -0500

    Agregado mensaje de error en CSS y JS

--- JS/app.js
+++ JS/app.js
@@ -7,7 +7,7 @@
 const nickInput = document.getElementById('nick');
 const sizeInput = document.getElementById('size');
 const formEntrada = document.getElementById('formEntrada');
-
+const error = document.getElementById('error');
 
 
 
@@ -17,11 +17,13 @@ function comprobarForm(e){
         console.log('El nick no puede estar vacio');
         nickInput.focus();
         e.preventDefault(); //Evita que se envie el formulario
+        error.innerText = 'El nick no puede estar vacio';
         return false;
     }else if(sizeInput.value == '0'){
         console.log('No se ha seleccionado un tamaño de tablero');
         sizeInput.focus();
         e.preventDefault(); //Evita que se envie el formulario
+        error.innerText = 'No se ha seleccionado un tamaño de tablero';
         return false;
     }
     return true;
